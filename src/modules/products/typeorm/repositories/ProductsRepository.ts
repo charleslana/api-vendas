74afb54aaff8f8a7bb15006e4e76e31a779f55f0
Charles Lana <charleslanop@gmail.com>
2020-12-25 09:41:04 -0300

    Create product repository

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,16 @@
+import {EntityRepository, Repository} from 'typeorm';
+import Product from "../entities/Product";
+
+@EntityRepository(Product)
+export class ProductsRepository extends Repository<Product> {
+
+    public async findByName(name: string): Promise<Product | undefined> {
+        const product = this.findOne({
+            where: {
+                name,
+            }
+        });
+
+        return product;
+    }
+}
