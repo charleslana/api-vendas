c323cc4b32ea65925bc64a096127ec9218104b85
Charles Lana <charleslanop@gmail.com>
2020-12-25 10:17:45 -0300

    Service create products

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,35 @@
+import {getCustomRepository} from "typeorm";
+import {ProductsRepository} from "../typeorm/repositories/ProductsRepository";
+import AppError from "../../../shared/errors/AppError";
+import Product from "../typeorm/entities/Product";
+
+interface InterfaceRequest {
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+class CreateProductService {
+
+    public async execute({name, price, quantity}: InterfaceRequest): Promise<Product> {
+        const productsRepository = getCustomRepository(ProductsRepository);
+        const productExists = await productsRepository.findByName(name);
+
+        if(productExists) {
+            throw new AppError('There is already one product with this name.')
+        }
+
+        const product = productsRepository.create({
+            name,
+            price,
+            quantity
+        });
+
+        await productsRepository.save(product);
+
+        return product;
+
+    }
+}
+
+export default CreateProductService;
