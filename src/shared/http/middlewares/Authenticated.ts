dc0449ccf94f9ecb7a41ac4e2370b5badc1024be
Charles Lana <charleslanop@gmail.com>
2020-12-25 18:34:38 -0300

    Add multer and fix

--- src/shared/http/middlewares/Authenticated.ts
+++ src/shared/http/middlewares/Authenticated.ts
@@ -3,7 +3,7 @@ import AppError from "../../errors/AppError";
 import {verify} from "jsonwebtoken";
 import authConfig from '../../../config/auth'
 
-interface TokenPayload {
+interface InterfaceTokenPayload {
     iat: number;
     exp: number;
     sub: string;
@@ -21,7 +21,7 @@ export default function Authenticated(request: Request, response: Response, next
     try {
         const decodedToken = verify(token, authConfig.jwt.secret);
 
-        const {sub} = decodedToken as TokenPayload;
+        const {sub} = decodedToken as InterfaceTokenPayload;
 
         request.user = {
             id: sub
