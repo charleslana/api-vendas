8d97c5cffdaaafc42bd246c144adb78c96d2dcc2
Charles Lana <charleslanop@gmail.com>
2020-12-25 10:23:00 -0300

    Service list product

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,17 @@
+import {getCustomRepository} from "typeorm";
+import {ProductsRepository} from "../typeorm/repositories/ProductsRepository";
+import Product from "../typeorm/entities/Product";
+
+class ListProductService {
+
+    public async execute(): Promise<Product[]> {
+        const productsRepository = getCustomRepository(ProductsRepository);
+
+        const products = productsRepository.find();
+
+        return products;
+
+    }
+}
+
+export default ListProductService;
