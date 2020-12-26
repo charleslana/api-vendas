2c4420271b8d1426dd1edb7c6b1a06bd74c22eb1
Charles Lana <charleslanop@gmail.com>
2020-12-26 00:20:36 -0300

    Fix upload validate

--- src/modules/users/controllers/UsersAvatarController.ts
+++ src/modules/users/controllers/UsersAvatarController.ts
@@ -1,9 +1,13 @@
 import {Request, Response} from "express";
 import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
+import AppError from "../../../shared/errors/AppError";
 
 export default class UsersAvatarController {
 
     public async update(request: Request, response: Response): Promise<Response> {
+        if(!request.file) {
+            throw new AppError('Invalid field, required avatar.');
+        }
         const updateAvatar = new UpdateUserAvatarService();
 
         const user = await updateAvatar.execute({
