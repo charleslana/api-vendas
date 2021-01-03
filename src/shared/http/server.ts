ff8bbaebc218c70a79def10ac3abcffb34af08c9
Charles <charleslanop@gmail.com>
2021-01-03 11:50:25 -0300

    Added pagination request list services

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -8,12 +8,15 @@ import AppError from "../errors/AppError";
 import '../typeorm';
 import uploadConfig from '../../config/upload';
 import multer from "multer";
+import {pagination} from 'typeorm-pagination';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use(pagination);
 
 app.use('/files', express.static(uploadConfig.directory));
 
